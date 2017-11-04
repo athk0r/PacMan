649d313e19b7c798e268ac9341cf019d266bb64f
athk0r <nick.osterhold@googlemail.com>
2017-11-04 22:45:47 +0100

    begin of newGame

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -2,10 +2,39 @@ package main;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Controller {
     @FXML
     AnchorPane anchorPane;
 
-    
+    @FXML
+    private void createNewGame(){
+        try {
+            BufferedReader input_file = new BufferedReader(new FileReader(new File("1_pacman_labyrinth.txt")));
+            String line = "";
+            int row = 0;
+            int column = 0;
+            ArrayList<String> arrLines = new ArrayList<>();
+            while((line = input_file.readLine()) != null){
+                row++;
+                arrLines.add(line);
+            }
+            column = arrLines.get(0).length();
+
+            GridPane gridPane = new GridPane();
+            //Fülle Feld
+            
+            anchorPane.getChildren().add(gridPane);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
